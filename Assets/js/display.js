a917f2510d477ace5efa1af7626f56f8d6c3bfe9
Mgamboa <marianellag400@gmail.com>
2022-11-05 20:18:56 -0500

    when user looks up city, cit shows behind scene, but on screen, it displays as undefined. will have to work on showing actual data + local storage

---
+++ Assets/js/display.js
@@ -0,0 +1,52 @@
+let apiKey = "bddbfb355365507f1fd422abd1989d50";
+let userForm = $('#user-form');
+let search = $('#search');
+let cityName = $('#city-name');
+let cityTemp = $('#city-temp');
+let windTemp = $('#wind-temp');
+// let userInput = $('#city-search');
+// let historyButtons = $('#historyBtns');
+let humidityTemp = $('#humidity-temp');
+// const kelvin = 0;
+// const celsius = kelvin - 273;
+// let fahrenheit = celsius * (9/5) +32;
+// fahrenheit = Math.floor(fahrenheit);<---math worked worked
+
+function getWeatherSearch() {
+  let citySearch = userForm.val();
+  let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}`;
+  getWeather(requestUrl);
+}
+
+function getWeather(requestUrl) {
+  fetch(requestUrl)
+  .then(function(data) {
+    console.log(data);
+    cityName.text(`City: ${data.name}`);
+    cityTemp.text(`Temp: ${Math.round(data - 273.15)} + °F`);
+    windTemp.text(`Wind: ${data.speed} MPH`);
+    humidityTemp.text(`Humidity: ${data.humidity}%`);
+  });
+}
+// Math.round(temperature - 273.15)
+// $(document).ready(function () {
+//   let searchHistory = JSON.parse(localStorage.getItem('userInput'));
+//   for (let i = 0; i < searchHistory.length; i++) {
+//       let searchAgain = $('<li>').text(searchHistory[i]);
+//       searchAgain.attr('class', 'prev-btns');
+//       historyButtons.append(searchAgain);
+//   }
+// });
+
+// searchBtn.on('click', geCity);
+// historyButtons.on('click', '.prev-btns', function (ev) {
+//   userInput.val($(ev.target).text());
+//   geCity();
+// });
+
+search.on('submit', function (event) {
+  event.preventDefault();
+  getWeatherSearch();
+  userForm.val('');
+});
+
